6e4ff116eb72e8ce96ac3c89784983ecc634e47e
Arsenya689 <simankov06@mail.ru>
2024-09-24 14:18:00 +0300

    Create ex 1,2,3 on git.py

---
+++ ex 1,2,3 on git.py
@@ -0,0 +1,27 @@
+# 1
+# integer_var = 9
+# float_var = 2.5
+# string_var = "Hello"
+# list_var = [5, 6, 7]
+# tuple_var = (5, 2)
+# dict_var = {2, 3, 4}
+set_var = {2, 3, 4}
+bool_var = True
+print(integer_var, float_var, string_var, list_var, tuple_var, dict_var, set_var, bool_var)
+
+# 2
+# name = "Senya"
+# age = 18
+# average_grade = 4.5
+# favorite_movies = ["Fight Club", "Scream", "Wasabi"]
+# personal_info = ("Simankov", "4", "4.5")
+# skills = {"Smoke": 100}
+# visited_countries = {"Sochi", "Dagestan"}
+# has_driver_license = False
+# print(name, age, average_grade, favorite_movies, personal_info, skills, visited_countries, has_driver_license)]
+
+# 3
+# processor = "Intel"
+# access_memory = 32
+# internet_speed = 119.9
+# print(processor, access_memory, internet_speed)
