32d20a61928f84921a2521cf0a3209e1ae2d3f83
Arsenya689 <simankov06@mail.ru>
2024-10-11 12:40:22 +0300

    Create ex 5 git.py

---
+++ ex 5 git.py
@@ -0,0 +1,24 @@
+# Задание 1
+#age = int(input())
+#if age < 18:
+#    print("Вы несовершеннолетний")
+#elif age > 18 and age <= 65:
+#    print("Вы трудоспособный человек")
+#else:
+#    print("Вы пенсионер")
+
+# Задание 2
+buy = int(input())
+if buy < 1000:
+    print("Скидка не предоставляется")
+elif buy > 1000 and buy <= 5000:
+    
+    print("Скидка 5%")
+else:
+    print("Скидка 10%")
+
+# Задание 3
+#print("\n[1]Сумма\n[2]")
+#summ = int(input("Выберите номер операции"))
+#if summ +:
+#    print("summ")
